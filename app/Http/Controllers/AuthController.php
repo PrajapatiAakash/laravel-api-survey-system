511d80bb8079a102f50790fc5588ca0737b0d623
Aakash Prajapati <prajapatiakash1021993@gmail.com>
2023-05-03 12:06:57 +0530

    feat: added the singup api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\SingupRequest;
+use App\Http\Requests\LoginRequest;
+
+class AuthController extends Controller
+{
+    public function singup(SingupRequest $request)
+    {
+
+    }
+
+    public function login(LoginRequest $request)
+    {
+        
+    }
+
+    public function logout(Request $request)
+    {
+        
+    }
+}
